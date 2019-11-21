244608cd01d0c17dfd00d7d630259831dba38a2a
Andrew Williams <1williamsaj@gmail.com>
2019-11-21 02:56:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
 
   def self.create(name:, grade:)
     student = Student.new(name, grade)
-    student
+    student.save
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
