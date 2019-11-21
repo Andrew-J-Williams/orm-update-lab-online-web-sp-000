ac8d45f43b94d2e60c6fab5f3e962e78361688b5
Andrew Williams <1williamsaj@gmail.com>
2019-11-21 02:46:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,8 @@ class Student
   attr_reader :id
 
   def initialize(name, grade, id=nil)
+
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
